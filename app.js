625b10c26700e6cb8fe2d42f13656df3b5b8b1a9
Nighat-Tariq-2009 <nighattariq1986@gmail.com>
2024-08-08 00:57:28 +0500

    bulb on/off project

---
+++ app.js
@@ -0,0 +1,12 @@
+var bulb = document.getElementById("bulb");
+var btnOn = document.getElementById("onbtn");
+var btnOff = document.getElementById("offbtn");
+
+
+function bulbOn(){
+  bulb.src = "images/bulb-on.png";
+}
+function bulbOff(){
+ bulb.src = "images/bulb-off.png";
+}
+
